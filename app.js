48a7d1a4c0767c208b8c66ff2bd6cf190a2f308e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-10-26 15:27:56 +0200

    Install appsignal

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const { Appsignal } = require("@appsignal/nodejs");
+
+const appsignal = new Appsignal({
+  active: true,
+  name: "OpenTelemetryExample",
+  apiKey: "e526dcd6-3d2e-41c7-91c4-4f6c3cedf7de",
+});
+
 const express = require("express");
+const { expressMiddleware } = require("@appsignal/express");
 
 const PORT = process.env.PORT || "8080";
 const app = express();
 
+app.use(expressMiddleware(appsignal));
+
 app.get("/", (req, res) => {
+  console.log(appsignal.tracer().currentSpan())
   res.send("Hello World");
 });
 
