5ddc3adb8ddd7f0c58aacfca793e08f902bf8fbd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-11-25 16:36:49 +0100

    Remove auto instrumentation

--- tracing.js
+++ tracing.js
@@ -1,10 +1,9 @@
 const opentelemetry = require("@opentelemetry/sdk-node");
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { ConsoleSpanExporter } = require("./ConsoleSpanExporter.js");
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new ConsoleSpanExporter(),
-  instrumentations: [getNodeAutoInstrumentations()]
+  instrumentations: []
 });
 
 sdk.start()
